06823bf36ce56241ee86f6eedc3023a7617ea12d
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-31 14:03:13 +0100

    testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
 console.log("test");
+
+const zahlen =[23,54, 75];
+
+zahlen.push(1,99,50,12,60);
+
+zahlen.unshift(0,0.001, 0.0001, 0.01, 0.1);
+
+zahlen.pop();
+zahlen.pop();
+
+zahlen.shift();
+zahlen.shift();
+
+console.log(zahlen);
+console.table(zahlen);
